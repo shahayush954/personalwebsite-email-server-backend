9cef097a1611b93051dd66506c6befefc4c48e64
ayush.sha <ayush.sha@internal.media.net>
2023-02-06 15:51:16 +0530

    added header checks in API

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const EmailSender = require('./utils/EmailSender');
 
+const AUTH_TOKEN_KEY = 'AUTH-TOKEN';
+const TOKEN_VALUE = 'nk4DYfZEBRME8CkMkXqNEf5ZM';
+
 
 const app = express();
 
@@ -9,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/sendMail', (request, response) => {
+  const headerValue = request.header(AUTH_TOKEN_KEY);
+
+  if (headerValue != TOKEN_VALUE) {
+    response.send("Invalid Access Token Value");
+    return;
+  }
   EmailSender.sendEmail(request.body)
     .then((res) => response.send(res))
     .catch((err) => response.send(err));
