00f6efe61b18785fa3eaceab525733b8853fc267
ayush.sha <ayush.sha@internal.media.net>
2023-02-11 16:57:32 +0530

    added sender email to text

--- index.js
+++ index.js
@@ -12,13 +12,35 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post('/sendMail', (request, response) => {
-  const headerValue = request.header(AUTH_TOKEN_KEY);
+  const headerValue = request.get(AUTH_TOKEN_KEY);
 
   if (headerValue != TOKEN_VALUE) {
     response.send("Invalid Access Token Value");
     return;
   }
-  EmailSender.sendEmail(request.body)
+
+  const options = request.body;
+  
+  if (!options.sender) {
+    response.send("Sender Address not found in the Options Object");
+  }
+
+  if (!options.to) {
+    response.send("To Address not found in the Options Object");
+  }
+
+  if (!options.subject) {
+    response.send("Subject not found in the Options Object");
+  }
+
+  if (!options.text) {
+    response.send("Email Text not found in the Options Object");
+  }
+
+  options.text = "From: " + options.sender + " \n" + options.text;
+  delete options.sender;
+
+  EmailSender.sendEmail(options)
     .then((res) => response.send(res))
     .catch((err) => response.send(err));
 });
--- utils/EmailSender.js
+++ utils/EmailSender.js
@@ -1,38 +1,25 @@
 const nodemailer = require('nodemailer');
 
+const NODEMAILER_EMAIL_ID = "qazxswedcvfr954@outlook.com";
+
 const transporter = nodemailer.createTransport({
   service: 'hotmail',
   auth: {
-    user: 'qazxswedcvfr954@outlook.com',
+    user: NODEMAILER_EMAIL_ID,
     pass: 'emailclient@954'
   }
 });
 
 // const options = {
-//   from: 'qazxswedcvfr954@outlook.com',
+//   from: NODEMAILER_EMAIL_ID,
 //   to: 'ayushzz459@gmail.com',
 //   subject: 'Attention: Test Email from Node Mailer',
 //   text: 'Hello! from Nodemailer.'
 // };
 
 const sendEmail = (options) => {
+  options.from = NODEMAILER_EMAIL_ID;
   return new Promise(function(resolve, reject){
-    if (!options.from) {
-      reject("From Address not found in the Options Object");
-    }
-
-    if (!options.to) {
-      reject("To Address not found in the Options Object");
-    }
-
-    if (!options.subject) {
-      reject("Subject not found in the Options Object");
-    }
-
-    if (!options.text) {
-      reject("Email Text not found in the Options Object");
-    }
-
     transporter.sendMail(options, function(err, info) {
       if (err) {
         reject(err);
